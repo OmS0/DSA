383698a638a751d79dbc9fb5bcaf9e0e18cd33dc
oms04 <oms041103@gmail.com>
2024-10-15 01:34:16 +0530

    Fibonacci Series

---
+++ src/fibonacci.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class fibonacci
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n1=0, n2=1, n3, i, c;
+        System.out.print("Enter The Count : ");
+        c=sc.nextInt();
+        System.out.print(n1+" "+n2);
+        for(i=2; i<c; i++)
+        {
+            n3=n1+n2;
+            System.out.print(" "+n3);
+            n1=n2;
+            n2=n3;
+        }
+
+    }
+}
